a20fc8e2c8de36b186e6d7e81318975fddc88ad0
stunstunstun <wjdsupj@gmail.com>
2021-12-05 13:04:08 +0900

    Initial the project

---
+++ src/main/kotlin/dietxurl/DietxurlApplication.kt
@@ -0,0 +1,11 @@
+package dietxurl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DietxurlApplication
+
+fun main(args: Array<String>) {
+	runApplication<DietxurlApplication>(*args)
+}
---
+++ src/test/kotlin/dietxurl/DietxurlApplicationTests.kt
@@ -0,0 +1,13 @@
+package dietxurl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DietxurlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
